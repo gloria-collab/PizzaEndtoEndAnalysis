0fef7cb97826ef0a8dd4468edf9a6e4f137acb6b
gloria-collab <george.rachel000@gmail.com>
2025-04-29 16:24:21 +0100

    crted a python data ingestion file and crted a folder for sql file

---
+++ sql_scripts/data_ingestion.sql
@@ -0,0 +1,19 @@
+CREATE TABLE orders AS (
+SELECT *
+FROM orders_2023
+UNION ALL
+SELECT *
+FROM orders_2024
+UNION ALL
+SELECT *
+FROM orders_2025);
+
+CREATE TABLE order_details AS (
+SELECT *
+FROM order_details_2023
+UNION ALL
+SELECT *
+FROM order_details_2024
+UNION ALL
+SELECT *
+FROM order_details_2025);
