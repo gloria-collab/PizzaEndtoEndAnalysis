f9c40c4eda40d656c20b38b5d8540ce7e8decb34
gloria-collab <george.rachel000@gmail.com>
2025-05-06 16:03:25 +0100

    COMMIT DATA ANAYSIS

--- sql_scripts/analysis.sql
+++ sql_scripts/analysis.sql
@@ -16,12 +16,12 @@ select *from order_details;
 select * from pizzas
 
 -- the most ordered pizza type
-select pt.name, p.pizza_id, sum(p.price * quantity) as revenue
+select pt.name, p.pizza_id,sum (p.price * quantity) as revenue
 from pizza_types as pt
 left join pizzas as p
 on p.pizza_type_id = pt.pizza_type_id
 left join order_details as od
 on od.pizza_id = p.pizza_id
 group by pt.name, p.pizza_id,p.price, od.quantity
-order by revenue desc limit;
+order by revenue desc ;
 
